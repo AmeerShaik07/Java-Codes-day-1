c083b0a646f6b1d4ce6d05dc8a85691801967028
Shaik Ameer <152466065+AmeerShaik07@users.noreply.github.com>
2025-04-28 14:01:06 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App{
+    static void showAppVersion(){
+        System.out.println("showAppVersion : 2.5.1");
+    }
+    public static void main(String[] args){
+        showAppVersion();
+    }
+}
---
+++ Store.java
@@ -0,0 +1,12 @@
+public class Store{
+    static void displayStoreHours(){
+        System.out.println("Store Hours : 9 Am - 9 PM");
+    }
+    static void displayStoreAddress(){
+        System.out.println("Store Address : 123 Main Street, NY");
+    }
+    public static void main(String[] args){
+        displayStoreHours();
+        displayStoreAddress();
+    }
+}
